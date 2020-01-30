bd64917475c70bcb9623cb04676e3b7b8610a34b
Ryosuke <ryosuke.san.hana@gmail.com>
2020-01-30 13:51:02 -0800

    :wrench: more customizations

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "test-site",
+  projectName: "codebook",
   organizationName: "facebook",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
@@ -46,7 +46,7 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: "img/favicon.ico",
+  headerIcon: "img/favicon.png",
   footerIcon: "img/favicon.ico",
   favicon: "img/favicon.ico",
 
